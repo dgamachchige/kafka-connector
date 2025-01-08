d0f6ba9e3ca6a1d22c996f527552a2ea5229cf4e
dgamachchige <dilanpriyankara@gmail.com>
2025-01-08 21:24:31 +1300

    Fixed failed test

--- src/test/java/com/dilang/kafka/kafka_connector/KafkaConnectorApplicationTests.java
+++ src/test/java/com/dilang/kafka/kafka_connector/KafkaConnectorApplicationTests.java
@@ -1,11 +1,16 @@
 package com.dilang.kafka.kafka_connector;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KafkaConnectorApplicationTests {
 
+	@BeforeAll
+	static void setUpEnvironment() {
+		System.setProperty("EVENTHUB_CONNECTION_STRING", "test-connection-string");
+	}
 	@Test
 	void contextLoads() {
 	}
